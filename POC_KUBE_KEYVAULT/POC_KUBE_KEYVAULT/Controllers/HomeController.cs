92ce022688531d6162a3e342ad83ec22939edb24
Wilson dos Santos <wilsonsantosnet@gmail.com>
2022-03-23 00:01:21 -0300

    quebrado

--- POC_KUBE_KEYVAULT/POC_KUBE_KEYVAULT/Controllers/HomeController.cs
+++ POC_KUBE_KEYVAULT/POC_KUBE_KEYVAULT/Controllers/HomeController.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace POC_KUBE_KEYVAULT.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        private static async Task<string> GetConnectionString()
         {
+
+            var kv_clientId = "ea7076c5-1712-4ea3-9c94-36223130eb07";
+            var kv_secretId = "L.z7Q~mIN1iEkEc5n.UYG9Yy30igdvKDLTVFZ";
+            var kv_url = "https://pockubekeyvault.vault.azure.net/";
+
+
+            var client2 = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
+                async (string auth, string resp, string scope) =>
+                {
+
+                    var authCtx = new AuthenticationContext(auth);
+                    var credencial = new ClientCredential(kv_clientId, kv_secretId);
+                    var result = await authCtx.AcquireTokenAsync(resp, credencial);
+                    if (result == null)
+                        throw new InvalidCastException("Erro");
+                    return result.AccessToken;
+
+                }));
+
+            var key2 = await client2.GetSecretAsync(kv_url, "ServiceBusCns");
+            return key2.Value;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var teste = await GetConnectionString();
+            ViewBag.Secret = teste;
             return View();
         }
 
