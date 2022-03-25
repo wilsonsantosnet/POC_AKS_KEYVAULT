95f7b58175f6a7ded3874488d03db90b407b3165
Wilson dos Santos <wilsonsantosnet@gmail.com>
2022-03-25 08:20:41 -0300

    ++

--- POC_KUBE_KEYVAULT/POC_KUBE_KEYVAULT/Controllers/HomeController.cs
+++ POC_KUBE_KEYVAULT/POC_KUBE_KEYVAULT/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.KeyVault;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.KeyVault;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
 using System.Threading.Tasks;
@@ -9,34 +11,27 @@ namespace POC_KUBE_KEYVAULT.Controllers
     public class HomeController : Controller
     {
 
-        private static async Task<string> GetConnectionString()
+        private string kv_clientId;
+        private string kv_secretId;
+        private string kv_url;
+        public HomeController()
         {
-
-            var kv_clientId = "ea7076c5-1712-4ea3-9c94-36223130eb07";
-            var kv_secretId = "L.z7Q~mIN1iEkEc5n.UYG9Yy30igdvKDLTVFZ";
-            var kv_url = "https://pockubekeyvault.vault.azure.net/";
-
-
-            var client2 = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
-                async (string auth, string resp, string scope) =>
-                {
-
-                    var authCtx = new AuthenticationContext(auth);
-                    var credencial = new ClientCredential(kv_clientId, kv_secretId);
-                    var result = await authCtx.AcquireTokenAsync(resp, credencial);
-                    if (result == null)
-                        throw new InvalidCastException("Erro");
-                    return result.AccessToken;
-
-                }));
-
-            var key2 = await client2.GetSecretAsync(kv_url, "ServiceBusCns");
-            return key2.Value;
+            this.kv_clientId = "ea7076c5-1712-4ea3-9c94-36223130eb07";
+            this.kv_secretId = "L.z7Q~mIN1iEkEc5n.UYG9Yy30igdvKDLTVFZ";
+            this.kv_url = "https://pockubekeyvault.vault.azure.net/";
         }
+
         public async Task<ActionResult> Index()
         {
-            var teste = await GetConnectionString();
-            ViewBag.Secret = teste;
+            var displayKeyKv = await GetConnectionString(this.kv_clientId, this.kv_secretId, this.kv_url);
+            var displayCertIdKv = await GetCertificate(this.kv_clientId, this.kv_secretId, this.kv_url);
+
+            var displayKeyAks = GetSecretFromEnvironment();
+            var displayMapsAks = GetMapsFromEnvironment();
+            var displayAppSettings = GetMapsFromAppSettings();
+
+
+            ViewBag.Display = $"{displayKeyKv} | {displayCertIdKv} | {displayKeyAks} | {displayMapsAks} | {displayAppSettings}";
             return View();
         }
 
@@ -53,5 +48,73 @@ namespace POC_KUBE_KEYVAULT.Controllers
 
             return View();
         }
+
+
+        #region Helpers
+       
+
+        private static async Task<string> GetConnectionString(string kv_clientId, string kv_secretId, string kv_url)
+        {
+            var client = GetClientKeyVault(kv_clientId, kv_secretId);
+            var key = await client.GetSecretAsync(kv_url, "ServiceBusCns");
+            return $"Secret From KV: {key.Value}";
+        }
+
+        private static async Task<string> GetCertificate(string kv_clientId, string kv_secretId, string kv_url)
+        {
+            var client2 = GetClientKeyVault(kv_clientId, kv_secretId);
+
+            var certificate = await client2.GetCertificateAsync(kv_url, "aks");
+            return $"Cert From KV: {certificate.Id}";
+        }
+
+        private static string GetSecretFromEnvironment()
+        {
+            return $"Secret From AKS: {Environment.GetEnvironmentVariable("testekey")}";
+        }
+
+        private static string GetMapsFromEnvironment()
+        {
+            return $"Maps From AKS: {Environment.GetEnvironmentVariable("cns")}";
+        }
+
+        private static string GetMapsFromAppSettings()
+        {
+            return $"App Settings: {System.Configuration.ConfigurationManager.AppSettings["cns"]}";
+        }
+
+        private static async Task<string> GetConnectionStringEnvironment(string kv_url)
+        {
+            //AZURE_CLIENT_SECRET
+            //AZURE_CLIENT_ID
+            //AZURE_TENANT_ID
+
+            var client = new SecretClient(
+                vaultUri: new Uri(kv_url),
+                credential: new EnvironmentCredential()
+            );
+
+            var key = await client.GetSecretAsync("ServiceBusCns");
+            return $"Secret From KV: {key.Value}";
+        }
+
+        private static KeyVaultClient GetClientKeyVault(string kv_clientId, string kv_secretId)
+        {
+            return new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
+                async (string auth, string resp, string scope) =>
+                {
+
+                    var authCtx = new AuthenticationContext(auth);
+                    var credencial = new ClientCredential(kv_clientId, kv_secretId);
+                    var result = await authCtx.AcquireTokenAsync(resp, credencial);
+                    if (result == null)
+                        throw new InvalidCastException("Erro");
+
+                    return result.AccessToken;
+
+                }));
+        }
+
+        #endregion
     }
 }
